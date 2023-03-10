6a0e10650a19158886826952fc352abf7fc72538
Sahil Anjum <1904219@kiit.ac.in>
2023-03-10 17:40:10 +0530

    static decision page added

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log(88);
+// console.log(88);
+let pScore = 0;
+let cScore = 0;
+// const hand = document.getElementsByClassName("paperButton");
+const hand = document.querySelector(".paperButton");
+const chooseOptio = document.querySelector(".choose-option");
+hand.addEventListener("click", () => {
+  chooseOptio.classList.add("fadeOut");
+  // match.classList.add("fadeIn");
+  console.log(99);
+});
