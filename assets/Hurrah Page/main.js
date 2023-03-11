d958b980d3f28731e9612d4137853146e66c970e
Sahil Anjum <1904219@kiit.ac.in>
2023-03-12 01:04:05 +0530

    added the local storage method to score the values after the page gets refreshed also

---
+++ assets/Hurrah Page/main.js
@@ -0,0 +1,17 @@
+const playbutton = document.querySelector(".playbutton");
+const Cut = document.querySelector(".Cut");
+const wholeRule = document.querySelector(".wholeRule");
+const rule1 = document.querySelector(".rule1");
+function mainpage() {
+  wholeRule.style.opacity = "0";
+  location.replace("../../inedx.html");
+}
+const cutRule = () => {
+  wholeRule.style.opacity = "0";
+};
+const rulePopup = () => {
+  wholeRule.style.opacity = "1";
+};
+playbutton.addEventListener("click", mainpage);
+rule1.addEventListener("click", rulePopup);
+Cut.addEventListener("click", cutRule);
--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 const matche = document.querySelector(".match");
 const chooseOptio = document.querySelector(".choose-option");
-
 const stoneButton = document.querySelector(".stoneButton");
 const scissorButton = document.querySelector(".scissorButton");
-// const paperButton = document.querySelector(".paperButton");
 const options = document.querySelectorAll(".options div");
-// const outerCircle1 = document.getElementsByClassName(".outerCircle1");
 const outerCircle1 = document.querySelector(".outerCircle1");
 const outerCircle2 = document.querySelector(".outerCircle2");
 const yhand = document.querySelector(".yourImage");
@@ -20,12 +17,15 @@ const ruleText = document.querySelector("#rule-text");
 const Cut = document.querySelector(".Cut");
 const wholeRule = document.querySelector(".wholeRule");
 const rule = document.querySelector(".rule");
-// const next = document.querySelector(".next");
+const yscore = document.querySelector(".yscore");
+const cscore = document.querySelector(".cscore");
+let pScore = 0;
+let cScore = 0;
 const game = () => {
-  let pScore = 0;
-  let cScore = 0;
-  //Play Match
-  //   matche.style.opacity = "0";
+  let Pscore = parseInt(localStorage.getItem("pscore"));
+  let Cscore = parseInt(localStorage.getItem("cscore"));
+  yscore.textContent = Pscore;
+  cscore.textContent = Cscore;
   const rulePopup = () => {
     wholeRule.style.opacity = "1";
   };
@@ -41,20 +41,10 @@ const game = () => {
         const computerChoice = computerOptions[computerNumber];
         console.log(this.id);
         console.log(computerChoice);
-        // setTimeout(() => {
-        //Here is where we call compare hands
-        // chooseOptio.classList.add("fadeOut");
         chooseOptio.style.opacity = "0";
         matche.style.opacity = "1";
         wholeRule.style.opacity = "0";
-        // chooseOptio.style.animation = "fadeOut ease";
-        // matche.style.animation = "fadeIn";
-        //   $(".choose-option").fadeOut();
-        // matche.classList.add("fadeIn");
-        //   $(".match").fadeIn(2000);
         compareHands(this.id, computerChoice);
-        //Update Images
-        // }, 2000);
       });
     });
   };
@@ -62,28 +52,42 @@ const game = () => {
   const cutRule = () => {
     wholeRule.style.opacity = "0";
   };
+
   Cut.addEventListener("click", cutRule);
+  const getscore = () => {
+    let Pscore = parseInt(localStorage.getItem("pscore"));
+    let Cscore = parseInt(localStorage.getItem("cscore"));
+    pScore = Pscore;
+    cScore = Cscore;
+  };
   const updateScore = () => {
     const yscore = document.querySelector(".yscore");
     const cscore = document.querySelector(".cscore");
-    console.log(pScore);
-    console.log(cScore);
-    yscore.textContent = pScore;
-    cscore.textContent = cScore;
+    localStorage.setItem("pscore", pScore.toString());
+    localStorage.setItem("cscore", cScore.toString());
+    // const scoreString1 = sessionStorage.getItem("pscore");
+    // const scoreString2 = sessionStorage.getItem("cscore");
+    // const Pscore = parseInt(scoreString1);
+    // const Cscore = parseInt(scoreString2);
+    let Pscore = parseInt(localStorage.getItem("pscore"));
+    let Cscore = parseInt(localStorage.getItem("cscore"));
+    yscore.textContent = Pscore;
+    cscore.textContent = Cscore;
   };
+
   playButtonn.addEventListener("click", () => {
     chooseOptio.style.opacity = "1";
     matche.style.opacity = "0";
     next.style.opacity = "0";
     ruleText.innerHTML = "RULES";
     wholeRule.style.opacity = "0";
-    // playMatch();
   });
+
   function Hurrah() {
     location.replace("assets/Hurrah Page/index.html");
   }
+
   const compareHands = (playerChoice, computerChoice) => {
-    //Update Text
     const decisionText = document.querySelector(".decisionText");
     const pcolours = computerColors[computerOptions.indexOf(playerChoice)];
     const ccolours = computerColors[computerOptions.indexOf(computerChoice)];
@@ -91,10 +95,6 @@ const game = () => {
     yhand.src = `./assets/${playerChoice}.png`;
     outerCircle1.style.borderColor = `${pcolours}`;
     outerCircle2.style.borderColor = `${ccolours}`;
-    // console.log(colours);
-
-    // console.log(colour);
-    //Checking for a tie
 
     if (playerChoice === computerChoice) {
       decisionText.textContent = "TIE UP";
@@ -109,7 +109,9 @@ const game = () => {
       rule.addEventListener("click", rulePopup);
       return;
     }
+
     //Check for Rock
+
     if (playerChoice === "rock") {
       if (computerChoice === "scissor") {
         decisionText.textContent = "YOU WIN";
@@ -119,9 +121,10 @@ const game = () => {
         pwinner.style.opacity = "0";
         ywinner.style.opacity = "1";
         next.style.opacity = "1";
-        ruleText.innerHTML = "RULES";
+        ruleText.innerHTML = "NEXT";
         next.addEventListener("click", rulePopup);
         rule.addEventListener("click", Hurrah);
+        getscore();
         pScore++;
         updateScore();
         return;
@@ -135,12 +138,15 @@ const game = () => {
         next.style.opacity = "0";
         ruleText.innerHTML = "RULES";
         rule.addEventListener("click", rulePopup);
+        getscore();
         cScore++;
         updateScore();
         return;
       }
     }
+
     //Check for Paper
+
     if (playerChoice === "paper") {
       if (computerChoice === "scissor") {
         decisionText.textContent = "YOU LOST";
@@ -152,6 +158,7 @@ const game = () => {
         next.style.opacity = "0";
         ruleText.innerHTML = "RULES";
         rule.addEventListener("click", rulePopup);
+        getscore();
         cScore++;
         updateScore();
         return;
@@ -166,12 +173,15 @@ const game = () => {
         ruleText.innerHTML = "NEXT";
         next.addEventListener("click", rulePopup);
         rule.addEventListener("click", Hurrah);
+        getscore();
         pScore++;
         updateScore();
         return;
       }
     }
+
     //Check for Scissors
+
     if (playerChoice === "scissor") {
       if (computerChoice === "rock") {
         decisionText.textContent = "YOU LOST";
@@ -183,6 +193,7 @@ const game = () => {
         next.style.opacity = "0";
         ruleText.innerHTML = "RULES";
         rule.addEventListener("click", rulePopup);
+        getscore();
         cScore++;
         updateScore();
         return;
@@ -197,6 +208,7 @@ const game = () => {
         ruleText.innerHTML = "NEXT";
         next.addEventListener("click", rulePopup);
         rule.addEventListener("click", Hurrah);
+        getscore();
         pScore++;
         updateScore();
         return;
@@ -204,10 +216,7 @@ const game = () => {
     }
   };
 
-  //Is call all the inner function
-  //   startGame();
   playMatch();
 };
 
-//start the game function
 game();
